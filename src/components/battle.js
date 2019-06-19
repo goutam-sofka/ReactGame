eec9cd0b337a5364f942b365a7f808bb435740d2
Vili <vtu@nix.mk>
2019-06-19 12:12:58 +0200

    made the dummy data better

--- src/components/battle.js
+++ src/components/battle.js
@@ -8,19 +8,46 @@ const Battle = ({ match, history }) => {
   const avenger = history.location.state.selectedAvengers
   const villain = history.location.state.selectedVillains
   
+  // const battle = [
+  //   {
+  //     num:1,
+  //     round: `${avenger[0].name} attacks ${villain[0].name},\n ${villain[0].name}'s HP is now ${villain[0].hp = villain[0].hp - avenger[0].atq}`
+  //   },
+  //   {
+  //     num: 2,
+  //     round: `${villain[1].name} attacks ${avenger[1].name}, \n ${avenger[1].name}'s HP is now ${avenger[1].hp = avenger[1].hp - villain[1].atq}`
+  //   },
+  //   {
+  //     num:3,
+  //     round: `${avenger[2].name} attacks ${villain[2].name}, \n ${villain[2].name}'s HP is now ${villain[2].hp = villain[2].hp - avenger[2].atq} `
+  //   }
+  // ]
+
   const battle = [
     {
-      num:1,
-      round: `${avenger[0].name} attacks ${villain[0].name},\n ${villain[0].name}'s HP is now ${villain[0].hp = villain[0].hp - avenger[0].atq}`
+      round: 1,
+      attacker: avenger[0].name,
+      attacked: villain[0].name,
+      attackedHp: villain[0].hp -= avenger[0].atq
     },
     {
-      num: 2,
-      round: `${villain[1].name} attacks ${avenger[1].name}, \n ${avenger[1].name}'s HP is now ${avenger[1].hp = avenger[1].hp - villain[1].atq}`
+      round: 2,
+      attacker: villain[1].name,
+      attacked: avenger[1].name,
+      attackedHp: avenger[0].hp -= villain[0].atq
     },
     {
-      num:3,
-      round: `${avenger[2].name} attacks ${villain[2].name}, \n ${villain[2].name}'s HP is now ${villain[2].hp = villain[2].hp - avenger[2].atq} `
-    }
+      round: 3,
+      attacker: avenger[2].name,
+      attacked: villain[2].name,
+      attackedHp: villain[2].hp -= avenger[2].atq
+    },
+    {
+      round: 4,
+      attacker: villain[0].name,
+      attacked: avenger[0].name,
+      attackedHp: avenger[0].hp -= villain[0].atq
+    },
   ]
   
   return (
@@ -29,9 +56,10 @@ const Battle = ({ match, history }) => {
         battle.map((battle, i) => {
           return (
             <Fragment>
-              <h2>Round {battle.num}</h2>
+              <h2>Round {battle.round}</h2>
               <div className='battle' key={i}>
-                <p>{battle.round}</p>
+                <p><b>{battle.attacker}</b> attacks <b>{battle.attacked}</b>. 
+                <br/><b>{battle.attacked}'s</b> HP is now <b>{battle.attackedHp}</b>. </p>
               </div>
             </Fragment>
           )
